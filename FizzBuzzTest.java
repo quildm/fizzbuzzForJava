416fdeb9bbb8f901d53fa722b1846af2ba4e7961
Dennis Quilantang <quildm@ymail.com>
2017-07-31 15:18:58 -0700

    created fizzBuzz for java

---
+++ FizzBuzzTest.java
@@ -0,0 +1,9 @@
+public class FizzBuzzTest {
+	public static void main(String[] args){
+		FizzBuzz iD = new FizzBuzz();
+		String fizzBuzz = iD.fizzBuzz(15);
+		System.out.println(fizzBuzz);
+	}
+}
+
+
